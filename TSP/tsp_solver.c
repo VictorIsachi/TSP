53d4efbf370233cefc65e604e17f4d8bec8a7563
VictorIsachi <victor.isachi@gmail.com>
2022-03-26 17:23:12 +0100

    Completed fig. 2 of Ch. 3.2

--- TSP/tsp_solver.c
+++ TSP/tsp_solver.c
@@ -293,7 +293,7 @@ static void comp_methods_1() {
 	fclose(perf_prof_file);
 }
 
-//chap. 3.2 
+//chap. 3.2 fig. 1
 static void comp_methods_2() {
 
 #if VERBOSE > 1
@@ -541,7 +541,191 @@ static void comp_methods_2() {
 	free(costs);
 
 	fclose(perf_prof_file);
+}
+
+//chap. 3.2 fig. 2
+static void comp_methods_3() {
+
+#if VERBOSE > 1
+	{ printf("Comparing greedy_random_start, greedy_random_start + 2-opt\n \
+			  extra-mileage_random_start, extra-mileage_random_start + 2-opt...\n");
+}
+#endif
+
+	tsp_instance_t greedy_inst_1;	//greedy random start
+	greedy_inst_1.time_limit = TOTAL_TIME_LIMIT / NUM_RERUNS;
+	greedy_inst_1.input_file_name = NULL;
+	greedy_inst_1.x_bound = -1;
+	greedy_inst_1.y_bound = -1;
+	greedy_inst_1.num_nodes = -1;
+	greedy_inst_1.random_seed = time(NULL);
+	greedy_inst_1.sol_procedure_flag = GREEDY;
+	greedy_inst_1.starting_index = -1;
+	greedy_inst_1.prob_ign_opt = 0.0;
+	greedy_inst_1.refine_flag = NO_REF;
+	greedy_inst_1.best_sol = NULL;
+	greedy_inst_1.best_sol_cost = DBL_INFY;
+	greedy_inst_1.costs = NULL;
+	greedy_inst_1.time_left = greedy_inst_1.time_limit;
+
+	tsp_instance_t greedy_inst_2;	//greedy random start + GRASP0.1
+	greedy_inst_2.time_limit = TOTAL_TIME_LIMIT / NUM_RERUNS;
+	greedy_inst_2.input_file_name = NULL;
+	greedy_inst_2.x_bound = -1;
+	greedy_inst_2.y_bound = -1;
+	greedy_inst_2.num_nodes = -1;
+	greedy_inst_2.random_seed = time(NULL);
+	greedy_inst_2.sol_procedure_flag = GREEDY;
+	greedy_inst_2.starting_index = -1;
+	greedy_inst_2.prob_ign_opt = 0.0;
+	greedy_inst_2.refine_flag = TWO_OPT;
+	greedy_inst_2.best_sol = NULL;
+	greedy_inst_2.best_sol_cost = DBL_INFY;
+	greedy_inst_2.costs = NULL;
+	greedy_inst_2.time_left = greedy_inst_2.time_limit;
+
+	tsp_instance_t extra_mileage_inst_1;	//extra-mileage random start
+	extra_mileage_inst_1.time_limit = TOTAL_TIME_LIMIT / NUM_RERUNS;
+	extra_mileage_inst_1.input_file_name = NULL;
+	extra_mileage_inst_1.x_bound = -1;
+	extra_mileage_inst_1.y_bound = -1;
+	extra_mileage_inst_1.num_nodes = -1;
+	extra_mileage_inst_1.random_seed = time(NULL);
+	extra_mileage_inst_1.sol_procedure_flag = EXTRA_MILEAGE;
+	extra_mileage_inst_1.starting_index = -1;
+	extra_mileage_inst_1.prob_ign_opt = 0.0;
+	extra_mileage_inst_1.refine_flag = NO_REF;
+	extra_mileage_inst_1.best_sol = NULL;
+	extra_mileage_inst_1.best_sol_cost = DBL_INFY;
+	extra_mileage_inst_1.costs = NULL;
+	extra_mileage_inst_1.time_left = extra_mileage_inst_1.time_limit;
+
+	tsp_instance_t extra_mileage_inst_2;	//extra-mileage random start + GRASP0.1
+	extra_mileage_inst_2.time_limit = TOTAL_TIME_LIMIT / NUM_RERUNS;
+	extra_mileage_inst_2.input_file_name = NULL;
+	extra_mileage_inst_2.x_bound = -1;
+	extra_mileage_inst_2.y_bound = -1;
+	extra_mileage_inst_2.num_nodes = -1;
+	extra_mileage_inst_2.random_seed = time(NULL);
+	extra_mileage_inst_2.sol_procedure_flag = EXTRA_MILEAGE;
+	extra_mileage_inst_2.starting_index = -1;
+	extra_mileage_inst_2.prob_ign_opt = 0.0;
+	extra_mileage_inst_2.refine_flag = TWO_OPT;
+	extra_mileage_inst_2.best_sol = NULL;
+	extra_mileage_inst_2.best_sol_cost = DBL_INFY;
+	extra_mileage_inst_2.costs = NULL;
+	extra_mileage_inst_2.time_left = extra_mileage_inst_2.time_limit;
+
+#if VERBOSE > 1
+	{ printf("Total time limit: %f...\nTime limit per instance %f...\n", TOTAL_TIME_LIMIT, TOTAL_TIME_LIMIT / NUM_RERUNS); }
+#endif
+
+	FILE* perf_prof_file = fopen(PERF_PROF_FILENAME, "w");
+	if (perf_prof_file == NULL) { fprintf(stderr, "cannot open the perf_prof_file\n"); exit(1); }
+
+	fprintf(perf_prof_file, "4, greedy_rand_start, greedy_rand_start+2-opt, extra-mileage_rand_start, extra-mileage_rand_start+2-opt\n");
+
+	srand(RAND_SEED); for (int i = 0; i < MIN_RAND_RUNS + log(1 + RAND_SEED); i++) rand();
+	point_2d_t* nodes = (point_2d_t*)malloc(NUM_POINTS * sizeof(point_2d_t));
+	int num_costs = NUM_POINTS * (NUM_POINTS - 1) / 2;
+	double* costs = (double*)malloc(num_costs * sizeof(double));
+	unsigned int random_seed;
+	for (int i = 0; i < NUM_RERUNS; i++) {
+
+		//generating the random nodes
+		for (int i = 0; i < NUM_POINTS; i++) {
+			nodes[i].x_coord = ((double)rand() / RAND_MAX) * POINT_BOUND;
+			nodes[i].y_coord = ((double)rand() / RAND_MAX) * POINT_BOUND;
+		}
+		greedy_inst_1.num_nodes = NUM_POINTS;
+		greedy_inst_2.num_nodes = NUM_POINTS;
+		greedy_inst_1.nodes = nodes;
+		greedy_inst_2.nodes = nodes;
+		extra_mileage_inst_1.num_nodes = NUM_POINTS;
+		extra_mileage_inst_2.num_nodes = NUM_POINTS;
+		extra_mileage_inst_1.nodes = nodes;
+		extra_mileage_inst_2.nodes = nodes;
+
+		//precomputing the costs
+		for (int i = 0; i < NUM_POINTS - 1; i++) {
+			for (int j = i + 1; j < NUM_POINTS; j++) {
+				costs[DIST_INDEX(i, j, NUM_POINTS)] = (double)((int)(dist(i, j, &greedy_inst_1) + 0.5));
+			}
+		}
+		greedy_inst_1.costs = costs;
+		greedy_inst_2.costs = costs;
+		extra_mileage_inst_1.costs = costs;
+		extra_mileage_inst_2.costs = costs;
+
+		//setting-up the random seeds
+		random_seed = rand();
+		greedy_inst_1.random_seed = random_seed;
+		greedy_inst_2.random_seed = random_seed;
+		extra_mileage_inst_1.random_seed = random_seed;
+		extra_mileage_inst_2.random_seed = random_seed;
+
+		//finding the best solutions
+		double greedy_best_sol_cost_1 = DBL_INFY;
+		greedy_inst_1.time_left = greedy_inst_1.time_limit;
+		while (greedy_inst_1.time_left > 0) {
+			double elapsed_timer_start = seconds();
+			tsp_opt(&greedy_inst_1);
+			ref_sol(&greedy_inst_1);
+			if (greedy_inst_1.best_sol_cost < greedy_best_sol_cost_1)
+				greedy_best_sol_cost_1 = greedy_inst_1.best_sol_cost;
+			free(greedy_inst_1.best_sol);
+			greedy_inst_1.best_sol = NULL;
+			double elapsed_timer_stop = seconds();
+			greedy_inst_1.time_left -= (elapsed_timer_stop - elapsed_timer_start);
+		}
+		double greedy_best_sol_cost_2 = DBL_INFY;
+		greedy_inst_2.time_left = greedy_inst_2.time_limit;
+		while (greedy_inst_2.time_left > 0) {
+			double elapsed_timer_start = seconds();
+			tsp_opt(&greedy_inst_2);
+			ref_sol(&greedy_inst_2);
+			if (greedy_inst_2.best_sol_cost < greedy_best_sol_cost_2)
+				greedy_best_sol_cost_2 = greedy_inst_2.best_sol_cost;
+			free(greedy_inst_2.best_sol);
+			greedy_inst_2.best_sol = NULL;
+			double elapsed_timer_stop = seconds();
+			greedy_inst_2.time_left -= (elapsed_timer_stop - elapsed_timer_start);
+		}
+		double extra_mileage_best_sol_cost_1 = DBL_INFY;
+		extra_mileage_inst_1.time_left = extra_mileage_inst_1.time_limit;
+		while (extra_mileage_inst_1.time_left > 0) {
+			double elapsed_timer_start = seconds();
+			tsp_opt(&extra_mileage_inst_1);
+			ref_sol(&extra_mileage_inst_1);
+			if (extra_mileage_inst_1.best_sol_cost < extra_mileage_best_sol_cost_1)
+				extra_mileage_best_sol_cost_1 = extra_mileage_inst_1.best_sol_cost;
+			free(extra_mileage_inst_1.best_sol);
+			extra_mileage_inst_1.best_sol = NULL;
+			double elapsed_timer_stop = seconds();
+			extra_mileage_inst_1.time_left -= (elapsed_timer_stop - elapsed_timer_start);
+		}
+		double extra_mileage_best_sol_cost_2 = DBL_INFY;
+		extra_mileage_inst_2.time_left = extra_mileage_inst_2.time_limit;
+		while (extra_mileage_inst_2.time_left > 0) {
+			double elapsed_timer_start = seconds();
+			tsp_opt(&extra_mileage_inst_2);
+			ref_sol(&extra_mileage_inst_2);
+			if (extra_mileage_inst_2.best_sol_cost < extra_mileage_best_sol_cost_2)
+				extra_mileage_best_sol_cost_2 = extra_mileage_inst_2.best_sol_cost;
+			free(extra_mileage_inst_2.best_sol);
+			extra_mileage_inst_2.best_sol = NULL;
+			double elapsed_timer_stop = seconds();
+			extra_mileage_inst_2.time_left -= (elapsed_timer_stop - elapsed_timer_start);
+		}
+
+		fprintf(perf_prof_file, "random_seed_%d, %f, %f, %f, %f\n", random_seed, greedy_best_sol_cost_1, greedy_best_sol_cost_2, \
+			extra_mileage_best_sol_cost_1, extra_mileage_best_sol_cost_2);
 	}
+	free(nodes);
+	free(costs);
+
+	fclose(perf_prof_file);
+}
 
 static void comp_methods_0() {
 	//compare the solution methods
@@ -687,8 +871,11 @@ int main(int argc, char const* argv[]) {
 	////uncomment for perf prof of chap 2.3////
 	//comp_methods_1();
 
-	////uncomment for perf prof of chap 3.2////
+	////uncomment for perf prof of chap 3.2 fig 1////
 	//comp_methods_2();
+
+	////uncomment for perf prof of chap 3.2 fig 2////
+	//comp_methods_3();
 
 	//performance comparisons among solution strategies greedy_random_start+GRASP0.1+2opt and extra-mileage_random_start+GRASP0.1+2opt
 	//comp_methods_0();
